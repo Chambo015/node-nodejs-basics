3f7b6906036c9f3c767282675e24792fe2083f84
Chambo015 <thegunner125@gmail.com>
2022-06-05 15:55:37 +0600

    feat: implement fs create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,18 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+let text = 'I am fresh and young'
+
 export const create = async () => {
-    // Write your code here 
+    fs.writeFile(path.join(__dirname, 'files', 'fresh.txt'),
+    text, {
+        flag: 'wx'
+    },
+    err => {
+        if (err) throw err 
+    })
 };
