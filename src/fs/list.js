4e427741d81633fe7c81cfdd9d98fa1eb6805ad6
Chambo015 <thegunner125@gmail.com>
2022-06-05 21:40:36 +0600

    feat: implement fs/list.js & fs/read.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,15 @@
+import { readdir } from 'fs/promises'
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const list = async () => {
-    // Write your code here 
+    try {
+        const arrFiles = await readdir(path.join(__dirname, 'files'))
+        console.log(arrFiles);
+    } catch (error) {
+        throw new Error('FS operation failed')
+    }
 };
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,15 @@
+import { readFile } from 'fs/promises'
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+    try {
+        const data = await readFile(path.join(__dirname, 'files', 'fileToRead.txt'), 'utf-8')
+        console.log(data)
+    } catch (error) {
+        throw new Error('FS operation failed')
+    }
 };
