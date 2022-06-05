184f14d10088c0ed6f9017a2b6bbb528bdb5e3a8
Chambo015 <thegunner125@gmail.com>
2022-06-05 21:21:07 +0600

    feat: implement fs/delete.js & fs/rename.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,14 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const remove = async () => {
-    // Write your code here 
+    try {
+        await fs.unlink(path.join(__dirname, 'files', 'fileToRemove.txt'))
+    } catch (error) {
+        throw new Error('FS operation failed')
+    }
 };
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,19 @@
+import { existsSync } from 'fs';
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let oldFile = path.join(__dirname, 'files', 'wrongFilename.txt');
+let newFile = path.join(__dirname, 'files', 'properFilename.md');
+
 export const rename = async () => {
-    // Write your code here 
+  try {
+    if(existsSync(newFile)) throw new Error('FS operation failed');
+    await fs.rename(oldFile, newFile);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
