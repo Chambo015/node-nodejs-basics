e6cc7877345148852e7c7374fc88a287badba7a2
Chambo015 <thegunner125@gmail.com>
2022-06-06 00:12:46 +0600

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,27 @@
+import { readFile } from 'fs/promises'
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const { createHash } = await import('crypto');
+      
+    const hash = createHash('sha256');
+    
+    hash.on('readable', () => {
+        const data = hash.read();
+        if (data) {
+            console.log(data.toString('hex'));
+        }
+    });
+
+    try {
+        const data = await readFile(path.join(__dirname, 'files', 'fileToCalculateHashFor.txt'), 'utf-8')
+        hash.write(data);
+        hash.end();
+    } catch (error) {
+        throw new Error('FS operation failed')
+    }
 };
