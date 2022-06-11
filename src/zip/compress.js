e37dccf839fa4336eaf3d5f2de6e610b8b300179
Chambo015 <thegunner125@gmail.com>
2022-06-11 22:27:40 +0600

    feat: implenet zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream } from 'fs'
+import { createGzip } from 'zlib'
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const compress = async () => {
-    // Write your code here 
+    const readable = createReadStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+    const writable = createWriteStream(path.join(__dirname, 'files', 'archive.gz'))
+    const gzip = createGzip()
+    readable.pipe(gzip).pipe(writable)
 };
+
+compress()
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,16 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream } from 'fs'
+import { createGunzip } from 'zlib'
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const decompress = async () => {
-    // Write your code here 
+    const readable = createReadStream(path.join(__dirname, 'files', 'archive.gz'));
+    const writable = createWriteStream(path.join(__dirname, 'files', 'fileToCompress.txt'))
+    const gzip = createGunzip()
+    readable.pipe(gzip).pipe(writable)
 };
+decompress()
