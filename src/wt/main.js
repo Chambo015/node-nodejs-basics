7fd23f6b5c0bd2dad15e4f3bfcb1be5e567072e7
Chambo015 <thegunner125@gmail.com>
2022-06-12 16:29:44 +0600

    feat: implement worker_threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,36 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const performCalculations = async () => {
-    // Write your code here
+    let nth = 10;
+    const arrayOfResults = await Promise.allSettled(cpus().map(() => {
+            return new Promise((resolve, reject) => {
+                const worker = new Worker(path.resolve(__dirname, './worker.js'), {
+                    workerData: nth++
+                });
+                worker.on('message', msg => {
+                    resolve({
+                        status: 'resolved',
+                        data: msg
+                    })
+                    
+                })
+                worker.on('error', e => {
+                    reject({
+                        status: 'error',
+                        data: null
+                    })
+                })
+            })
+        }
+    )) 
+    const parseResult = arrayOfResults.map(e => e.value)
+    console.log(parseResult);
+    
 };
+performCalculations()
