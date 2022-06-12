1cdc6e4bdda51abb60acd01926fd1aabfff3f5a1
Chambo015 <thegunner125@gmail.com>
2022-06-12 18:35:34 +0600

    feat: implement child_process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,29 @@
+import { spawn, fork } from 'child_process'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    // Use spawn
+    /* 
+    const child = spawn('node', [path.resolve(__dirname, 'files', 'script.js'), ...args])
+    process.stdin.on('data', chunk => {
+        child.stdin.write(chunk)
+    })
+    child.stdout.on('data', chunk => {
+        process.stdout.write(chunk.toString())
+    }) 
+    */
+
+    // Use fork
+    const child = fork(path.resolve(__dirname, 'files', 'script.js'), args, { stdio: 'pipe'})
+    process.stdin.on('data', chunk => {
+        child.stdin.write(chunk)
+    })
+    child.stdout.on('data', chunk => {
+        process.stdout.write(chunk.toString())
+    })
 };
+spawnChildProcess(process.argv.slice(2))
